ad4411f7079a402f542bfc6f7487a891aef75997
Toshin <toshintuli@gmail.com>
2022-02-10 23:32:55 +0530

    first draft

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// mongoose.connect("");
+
+const app=express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+    
+});
+
+app.get("/:acc",function(req,res){
+    res.render("account");
+});
+
+app.post("/confirm",function(req,res){
+    var a=req.body.pswd1;
+    var b=req.body.pswd2;
+    console.log(req.body);
+    if(a!=b){
+        res.redirect("/register");
+    }else{
+        res.send("Successfull");
+    }
+})
+app.listen(3000,function(){
+    console.log("Listening to port 3000");
+});
