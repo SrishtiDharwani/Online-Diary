8577dec3a78eb854b13bb948046672158919317c
SrishtiDharwani <srishti.dharwani.30@gmail.com>
2022-02-12 04:15:40 +0530

    drafted intro page with carousel and navbar

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//jshint eversion:6
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -15,7 +16,6 @@ app.get("/",function(req,res){
 
 app.get("/register",function(req,res){
     res.render("register");
-    
 });
 
 app.get("/:acc",function(req,res){
@@ -25,11 +25,14 @@ app.get("/:acc",function(req,res){
 app.post("/confirm",function(req,res){
     var a=req.body.pswd1;
     var b=req.body.pswd2;
-    console.log(req.body);
-    if(a!=b){
+    // console.log(req.body);
+    if(a!=b)
+    {
         res.redirect("/register");
-    }else{
-        res.send("Successfull");
+    }
+    else
+    {
+        res.render("intro");
     }
 })
 app.listen(3000,function(){
