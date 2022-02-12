c7eb8cc7440e9cf6059ffb32d00a94dd643dcded
SrishtiDharwani <srishti.dharwani.30@gmail.com>
2022-02-12 22:52:52 +0530

    modified carousel

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// mongoose.connect("");
+// mongoose.connect("mongodb://27017/entries", {useNewUrlParser:true});
 
 const app=express();
 app.set("view engine", "ejs");
